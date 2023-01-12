e385e6f73febb4d1fec541114f168fdddb3e988f
BryanCorret <bryan.corret@gmail.com>
2023-01-12 22:45:12 +0100

    ajour connexion/reception , fonctions, mp , déconnexion

---
+++ SRC/Commande.java
@@ -0,0 +1,29 @@
+
+import java.io.*;
+
+public class Commande implements Serializable {
+
+	// The different types of message sent by the Client
+	// WHO_HERE recevoir la liste des utilisateurs connectés
+	// MESSAGE message privée
+	// LOGOUT se déconnecter
+	static final int WHO_HERE = 0;
+	static final int MESSAGE = 1;
+	static final int LOGOUT = 2;
+	private int type;
+	private String message;
+	
+	// constructor
+	Commande(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	int getType() {
+		return type;
+	}
+
+	String getMessage() {
+		return message;
+	}
+}
